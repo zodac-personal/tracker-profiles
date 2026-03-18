86be1a772e901b1cb956c49674e6501665b8d191
zodac <arouge110@msn.com>
2026-03-19 02:54:00 +1300

    [NordicHD] Updating for new UI

--- tracker-profiles-screenshots/src/main/java/net/zodac/tracker/handler/NordicHd.java
+++ tracker-profiles-screenshots/src/main/java/net/zodac/tracker/handler/NordicHd.java
@@ -19,21 +19,11 @@ package net.zodac.tracker.handler;
 
 import static net.zodac.tracker.framework.xpath.HtmlElement.a;
 import static net.zodac.tracker.framework.xpath.HtmlElement.div;
-import static net.zodac.tracker.framework.xpath.HtmlElement.form;
 import static net.zodac.tracker.framework.xpath.HtmlElement.input;
-import static net.zodac.tracker.framework.xpath.HtmlElement.li;
-import static net.zodac.tracker.framework.xpath.HtmlElement.span;
-import static net.zodac.tracker.framework.xpath.HtmlElement.table;
-import static net.zodac.tracker.framework.xpath.HtmlElement.tbody;
-import static net.zodac.tracker.framework.xpath.HtmlElement.td;
-import static net.zodac.tracker.framework.xpath.HtmlElement.tr;
-import static net.zodac.tracker.framework.xpath.HtmlElement.ul;
 import static net.zodac.tracker.framework.xpath.XpathAttributePredicate.atIndex;
-import static net.zodac.tracker.framework.xpath.XpathAttributePredicate.withAttribute;
-import static net.zodac.tracker.framework.xpath.XpathAttributePredicate.withId;
+import static net.zodac.tracker.framework.xpath.XpathAttributePredicate.withClass;
+import static net.zodac.tracker.framework.xpath.XpathAttributePredicate.withType;
 
-import java.util.Collection;
-import java.util.List;
 import net.zodac.tracker.framework.annotation.TrackerHandler;
 import net.zodac.tracker.framework.xpath.XpathBuilder;
 import org.openqa.selenium.By;
@@ -41,65 +31,39 @@ import org.openqa.selenium.By;
 /**
  * Implementation of {@link AbstractTrackerHandler} for the {@code NordicHD} tracker.
  */
-@TrackerHandler(name = "NordicHD", url = "http://cinefilhd.eu/")
+@TrackerHandler(name = "NordicHD", url = "https://nordichd.org/")
 public class NordicHd extends AbstractTrackerHandler {
 
     @Override
     protected By usernameFieldSelector() {
-        return By.id("nickname");
+        return By.id("loginbox_membername");
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * <p>
-     * For {@link NordicHd}, there are multiple buttons for login. While only one of them has the value 'LOGIN', all of them seem to work, so
-     * for now we just take the first one.
-     *
-     * @return the login button {@link By} selector
-     */
+    @Override
+    protected By passwordFieldSelector() {
+        return By.id("loginbox_password");
+    }
+
     @Override
     protected By loginButtonSelector() {
         return XpathBuilder
-            .from(form, withAttribute("method", "post"))
-            .child(table, atIndex(1))
-            .child(tbody, atIndex(1))
-            .child(tr, atIndex(6))
-            .child(td, atIndex(1))
-            .child(input, atIndex(1))
+            .from(input, withClass("submit"), withType("submit"))
             .build();
     }
 
     @Override
     protected By profilePageSelector() {
         return XpathBuilder
-            .from(div, withId("statusbar"))
-            .child(div, atIndex(1))
-            .child(span, atIndex(1))
+            .from(div, withClass("eLinks"))
             .child(a, atIndex(1))
             .build();
     }
 
     @Override
-    protected Collection<By> ipAddressElements() {
-        return List.of(
-            XpathBuilder
-                .from(div, withId("general"))
-                .child(table, atIndex(1))
-                .child(tbody, atIndex(1))
-                .child(tr, atIndex(4))
-                .child(td, atIndex(2))
-                .build()
-        );
-    }
-
-    @Override
     protected By logoutButtonSelector() {
         return XpathBuilder
-            .from(div, withId("gm_taps_right"))
-            .child(ul, atIndex(1))
-            .child(li, atIndex(11))
-            .child(a, atIndex(1))
+            .from(div, withClass("eLinks"))
+            .child(a, atIndex(2))
             .build();
     }
 }
