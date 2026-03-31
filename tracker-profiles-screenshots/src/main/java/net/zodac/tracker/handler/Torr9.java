e255244c9f31985912fec3dac2033419c1e93760
zodac <arouge110@msn.com>
2026-04-01 00:51:04 +1300

    [Torr9] Implemented tracker

---
+++ tracker-profiles-screenshots/src/main/java/net/zodac/tracker/handler/Torr9.java
@@ -0,0 +1,119 @@
+/*
+ * BSD Zero Clause License
+ *
+ * Copyright (c) 2024-2026 zodac.net
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package net.zodac.tracker.handler;
+
+import static net.zodac.tracker.framework.xpath.HtmlElement.a;
+import static net.zodac.tracker.framework.xpath.HtmlElement.button;
+import static net.zodac.tracker.framework.xpath.HtmlElement.div;
+import static net.zodac.tracker.framework.xpath.HtmlElement.footer;
+import static net.zodac.tracker.framework.xpath.HtmlElement.header;
+import static net.zodac.tracker.framework.xpath.HtmlElement.input;
+import static net.zodac.tracker.framework.xpath.HtmlElement.li;
+import static net.zodac.tracker.framework.xpath.HtmlElement.ul;
+import static net.zodac.tracker.framework.xpath.XpathAttributePredicate.atIndex;
+import static net.zodac.tracker.framework.xpath.XpathAttributePredicate.withName;
+import static net.zodac.tracker.framework.xpath.XpathAttributePredicate.withType;
+
+import java.util.Collection;
+import java.util.List;
+import net.zodac.tracker.framework.annotation.TrackerHandler;
+import net.zodac.tracker.framework.xpath.NamedHtmlElement;
+import net.zodac.tracker.framework.xpath.XpathBuilder;
+import net.zodac.tracker.handler.definition.HasFixedHeader;
+import org.openqa.selenium.By;
+
+/**
+ * Implementation of {@link AbstractTrackerHandler} for the {@code Torr9} tracker.
+ */
+@TrackerHandler(name = "Torr9", url = "https://torr9.net/")
+public class Torr9 extends AbstractTrackerHandler implements HasFixedHeader {
+
+    @Override
+    protected By usernameFieldSelector() {
+        return XpathBuilder
+            .from(input, withName("username"), withType("text"))
+            .build();
+    }
+
+    @Override
+    protected By passwordFieldSelector() {
+        return XpathBuilder
+            .from(input, withName("password"), withType("password"))
+            .build();
+    }
+
+    @Override
+    protected By loginButtonSelector() {
+        return XpathBuilder
+            .from(button, withType("submit"))
+            .build();
+    }
+
+    @Override
+    protected By profilePageSelector() {
+        return XpathBuilder
+            .from(footer, atIndex(1))
+            .child(div, atIndex(1))
+            .child(div, atIndex(1))
+            .child(div, atIndex(4))
+            .child(ul, atIndex(1))
+            .child(li, atIndex(1))
+            .child(a, atIndex(1))
+            .build();
+    }
+
+    @Override
+    protected By profilePageContentSelector() {
+        return By.tagName("code");
+    }
+
+    @Override
+    public By headerSelector() {
+        return XpathBuilder
+            .from(header, atIndex(1))
+            .build();
+    }
+
+    @Override
+    protected Collection<By> emailElements() {
+        return List.of(
+            XpathBuilder
+                .from(input, withType("email"))
+                .build()
+        );
+    }
+
+    @Override
+    protected Collection<By> torrentPasskeyElements() {
+        return List.of(
+            XpathBuilder
+                .from(NamedHtmlElement.of("code"))
+                .build()
+        );
+    }
+
+    @Override
+    protected By logoutButtonSelector() {
+        return XpathBuilder
+            .from(header, atIndex(1))
+            .child(div, atIndex(1))
+            .child(div, atIndex(2))
+            .child(button, atIndex(1))
+            .build();
+    }
+}
