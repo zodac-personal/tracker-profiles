60bc2dc89a2cb7777bc07e9c4c7ad9913cf6ac80
zodac <arouge110@msn.com>
2026-05-17 16:26:04 +1200

    [CI] Not running pre-commit hook unless current branch is 'master'

--- ci/hooks/pre-commit.sh
+++ ci/hooks/pre-commit.sh
@@ -2,6 +2,12 @@
 
 trap 'echo; exit 130' INT
 
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "${current_branch}" != "master" ]; then
+  echo "Skipping pre-commit checks (branch: ${current_branch})"
+  exit 0
+fi
+
 echo "Running full build with tests and lints"
 if ! .github/scripts/lint_and_tests.sh; then
   echo "Pre-commit build failed, commit aborted"
